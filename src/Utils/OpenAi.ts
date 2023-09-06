356a3633de809d74aa45f3808876a0515470a263
Samir Merrach <89661483+SamMrach@users.noreply.github.com>
2023-09-06 13:06:18 +0100

    Update OpenAi.ts

--- src/Utils/OpenAi.ts
+++ src/Utils/OpenAi.ts
@@ -1,7 +1,7 @@
 import OpenAI from 'openai';
 
 const openai = new OpenAI({
-  apiKey: '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', // defaults to process.env["OPENAI_API_KEY"],
+  apiKey: 'openai_api_key', // defaults to process.env["OPENAI_API_KEY"],
   dangerouslyAllowBrowser: true
 });
 
